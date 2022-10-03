bd6b9f4e6e2a72e4c2870502096279ce7a3fb91e
Deathlymad <meyerphilip96@googlemail.com>
2022-10-03 19:34:49 +0200

    added equidistant placing of pillars according to order

--- Assets/Components/EquidistantFluffDistributor.cs
+++ Assets/Components/EquidistantFluffDistributor.cs
@@ -9,20 +9,23 @@ public class EquidistantFluffDistributor : MonoBehaviour
     public GameObject referenceCenter;
     public float distance;
     public Vector3 offset;
+    public float rots = 1.0f;
+    public float rotOffset = 0.0f;
     // Start is called before the first frame update
     void Awake()
     {
         float factor = 1.0f / transform.childCount;
-        float pos = 0.0f;
+        float pos = rotOffset;
         foreach (Transform child in transform)
         {
-            child.position = new Vector3((float)Math.Cos(2 * Math.PI * pos), 0.0f, (float)Math.Sin(2 * Math.PI * pos)) * distance + referenceCenter.transform.position + offset;
+            child.position = new Vector3((float)Math.Cos(2 * Math.PI * pos * rots), 0.0f, (float)Math.Sin(2 * Math.PI * pos * rots)) * distance + referenceCenter.transform.position + offset;
 
             child.up = Vector3.Normalize(referenceCenter.transform.position - child.position);
             pos += factor;
 
             //other setup
-            child.gameObject.GetComponent<Animator>().enabled = false;
+            if (child.gameObject.GetComponent<Animator>() != null)
+                child.gameObject.GetComponent<Animator>().enabled = false;
         }
     }
 }
