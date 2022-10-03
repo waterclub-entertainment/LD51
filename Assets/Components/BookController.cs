7101755033b28840163f429aa8af5a80d5db27e1
MDeiml <matthias@deiml.net>
2022-10-03 19:14:47 +0200

    More picking up

--- Assets/Components/BookController.cs
+++ Assets/Components/BookController.cs
@@ -25,6 +25,28 @@ public class BookController : MonoBehaviour {
         currentPage--;
     }
     
+    public void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            bool didHit = false;
+            if (Physics.Raycast(ray, out hitInfo)) {
+                if (hitInfo.collider.transform.parent?.parent == transform) {
+                    if (hitInfo.collider.transform.GetSiblingIndex() == 0) {
+                        PreviousPage();
+                    } else {
+                        NextPage();
+                    }
+                    didHit = true;
+                }
+            }
+            if (!didHit) {
+                transform.parent.GetComponent<Collider>().enabled = true;
+                transform.parent.GetComponent<Animator>().SetTrigger("Zoom");
+            }
+        }
+    }
+    
     public void UpdateImages() {
         image.materials[0] = images[currentPage];
         if (currentPage != images.Length - 1) {
